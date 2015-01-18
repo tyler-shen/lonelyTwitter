63a1798a9ab8613a8650a22dc29666c9bc3b280c
Tiancheng Shen <tshen@cs.ualberta.ca>
2015-01-18 16:14:04 -0700

    second edition

--- src/ca/ualberta/cs/lonelytwitter/Friends.java
+++ src/ca/ualberta/cs/lonelytwitter/Friends.java
@@ -1,12 +1,27 @@
 package ca.ualberta.cs.lonelytwitter;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class Friends extends User {
 	
-	private ArrayList<User> array = new ArrayList<User>();
+	private ArrayList<User> friendlist = new ArrayList<User>();
 	
-	public void 
+	public void addFriend(User name) {
+		friendlist.add(name);
+	}
 	
+	public void deleteFriend(User name){
+		friendlist.remove(name);
+	}
+	
+	public void showFriend(){
+		if (friendlist.isEmpty()){
+			System.out.println("You friend list is empty.");
+		}
+		else{
+			for (User value:friendlist){
+				System.out.println(value);
+			}
+		}
+	}
 }
